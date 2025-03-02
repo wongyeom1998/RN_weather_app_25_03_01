1654dd9ddda1c4e57005eb7c280d1dbcf27064be
wongyeom1998 <dnjsrua0514@gmail.com>
2025-03-02 15:18:09 +0900

    날씨 APP 레이아웃 구성 및 디자인

--- App.js
+++ App.js
@@ -6,7 +6,18 @@ export default function App() {
   const [number, setNumber] = useState(0)
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 50}}>리액트 네이티브</Text>
+      <View style={styles.cityCon}>
+        <Text style={styles.city}>Busan</Text>
+      </View>
+      <View style={styles.weatherCon}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>3월 13일, 일, 15:05</Text>
+          <Text style={styles.desc}>맑음</Text>
+        </View> 
+        <View style={styles.tempCon}>
+          <Text style={styles.temp}>24</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +26,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffe01a',
   },
+  cityCon: {
+    flex: 1,
+  },
+  city: {
+    flex: 1,
+    marginTop : 50,
+    fontSize : 40,
+    textAlign: "center",
+    fontWeight : "bold",
+    paddingTop : 20,
+  },
+  weatherCon : {
+    flex : 3,
+  },
+  day:{
+    flex : 0.2,
+    alignItems : "center",
+    justifyContent : "center"
+  },
+  regDate : {
+    paddingTop : 10,
+    paddingLeft : 20,
+    paddingRight : 20,
+    paddingBottom : 15,
+    backgroundColor : "black",
+    color : "white",
+    borderRadius : 20,
+    fontWeight : "bold"
+  },
+  desc : {
+    marginTop : 20,
+    fontSize : 20,
+    fontWeight : "bold",
+
+
+  },
+  tempCon : {
+    flex : 0.3,
+    alignItems : "center",
+    justifyContent : "center",
+    
+
+  },
+  temp: {
+    fontSize : 120,
+  }
+
 });
